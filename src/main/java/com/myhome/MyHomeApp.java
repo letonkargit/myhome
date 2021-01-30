9397f45415c72431ddef1160fe123ae5392fc0ce
onkar <onkargurav777@gmail.com>
2021-01-29 22:51:12 -0500

    First commit

---
+++ src/main/java/com/myhome/MyHomeApp.java
@@ -0,0 +1,11 @@
+package com.myhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHomeApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MyHomeApp.class, args);
+    }
+}
